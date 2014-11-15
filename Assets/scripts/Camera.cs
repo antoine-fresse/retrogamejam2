65c1400942859a08c155bc95c2c98575265bfac7
Antoine FRESSE <afresse54@gmail.com>
2014-11-15 20:23:31 +0100

    Explosions !

--- Assets/scripts/Camera.cs
+++ Assets/scripts/Camera.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Camera : MonoBehaviour {
 
+	public static Camera instance;
+	public float shake = 0.0f;
+
+	private Vector3 position;
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		instance = this;
+		position = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		float t = Time.time;
+		shake = Mathf.Max(shake - shake*0.5f, 0.0f);
+
+		transform.position = position + new Vector3(Mathf.Cos(20 * t) * shake * 0.2f, Mathf.Cos(50 * t + 3) * shake * 0.2f, 0.0f);
 	}
 }
---
+++ Assets/scripts/Explosion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.instance.shake += transform.localScale.x * 3.0f;
+		StartCoroutine(Expire());
+	}
+
+	IEnumerator Expire() {
+		yield return new WaitForSeconds(1.0f);
+		Destroy(gameObject);
+	}
+	
+}
