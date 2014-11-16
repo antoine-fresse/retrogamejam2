894fb86d1c5a029386123e8601b88debbd1daf61
Benoit <dumesnil.benoit@gmail.com>
2014-11-16 12:49:28 +0100

    Amélioration des MBExplosion.

---
+++ Assets/scripts/ExplosionTooltip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionTooltip : MonoBehaviour {
+
+    public int m_duration;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator disapear() {
+        yield return new WaitForSeconds(m_duration);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/scripts/MBExplosion.cs
+++ Assets/scripts/MBExplosion.cs
@@ -7,25 +7,33 @@ public class MBExplosion : MonoBehaviour {
 	public int count = 5;
 	public float interval = 0.25f;
 
+    public Vector3 m_Direction;
+    private Vector3 m_CrossDirection;
+
 	public ExplosionCluster prefabExplosion;
 
 
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(Explosion());
+
+        m_CrossDirection = new Vector3(m_Direction.y, -m_Direction.x, 0.0f);
+        m_CrossDirection.Normalize();
+
+        m_Direction.Normalize();
 	}
 
 	IEnumerator Explosion() {
 
 		for (int i = 0; i < count; i++) {
 
-			var exp = Instantiate(prefabExplosion, transform.position + new Vector3(-1,i,0), Quaternion.identity) as ExplosionCluster;
+            var exp = Instantiate(prefabExplosion, transform.position + (m_Direction * i) + m_CrossDirection, Quaternion.identity) as ExplosionCluster;
 
 			exp.interval = 0.05f;
 			exp.count = 3;
 			exp.radius = 0.5f;
 
-			exp = Instantiate(prefabExplosion, transform.position + new Vector3(1, i, 0), Quaternion.identity) as ExplosionCluster;
+            exp = Instantiate(prefabExplosion, transform.position + (m_Direction * i) - m_CrossDirection, Quaternion.identity) as ExplosionCluster;
 
 			exp.interval = 0.05f;
 			exp.count = 3;
